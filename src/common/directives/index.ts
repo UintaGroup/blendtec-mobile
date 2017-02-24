ef21a4c9510958beddd7a7c2608fcb2da44a0899
Stone C. Lasley <stonelasley@gmail.com>
2017-02-23 23:01:29 -0700

    tutorial icons

--- src/common/directives/index.ts
+++ src/common/directives/index.ts
@@ -1,12 +1,15 @@
 import { BlendtecButton }   from './bt-button.component';
 import { Logo }             from './logo.component';
+import { Swirl }            from "./swirl.component";
 
 export {
 	Logo,
-	BlendtecButton
+	BlendtecButton,
+	Swirl
 }
 
 export const COMMON_DIRECTIVES = [
 	Logo,
+	Swirl,
 	BlendtecButton
 ];
