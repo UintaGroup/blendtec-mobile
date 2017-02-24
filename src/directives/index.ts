b02e30c94a71b853f3ede8031d02cf6f049d441b
Stone C. Lasley <stonelasley@gmail.com>
2017-02-23 21:11:28 -0700

    bt-button

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,9 +1,12 @@
-import { LogoDirective }    from './logo';
+import { LogoDirective }        from './logo.directive';
+import { BtButtonDirective }    from './bt-button.directive';
 
 export {
 	LogoDirective,
+	BtButtonDirective
 }
 
 export const BLENDTEC_DIRECTIVES = [
-	LogoDirective
+	LogoDirective,
+	BtButtonDirective
 ];
